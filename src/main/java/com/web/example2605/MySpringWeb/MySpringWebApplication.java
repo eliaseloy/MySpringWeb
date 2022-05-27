72052cea04821f511d3b6d9b19d0c50ac2e79063
Elias Eloy <eliaseloylima@gmail.com>
2022-05-26 21:52:15 -0300

    Commit #01
    projeto web exemplo utilizando Spring + Java + MySQL + Maven +

---
+++ src/main/java/com/web/example2605/MySpringWeb/MySpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.web.example2605.MySpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/example2605/MySpringWeb/MySpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.example2605.MySpringWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
